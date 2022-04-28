f4d94f21f17d0bb0801c2332cff86fd0b7abde14
Thanh Tran <70768748+thanhtranlk@users.noreply.github.com>
2022-04-28 00:13:47 -0400

    Add files via upload

---
+++ myconfig.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDEXphHoYZXSNurH6ewHfMGf7iQEeF9zjA",
+  authDomain: "final-project-671a6.firebaseapp.com",
+  projectId: "final-project-671a6",
+  storageBucket: "final-project-671a6.appspot.com",
+  messagingSenderId: "895774743858",
+  appId: "1:895774743858:web:0b232c6358aa5411f54b2b"
+  };
+  
